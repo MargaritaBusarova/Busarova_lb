6daa39c983fb2b3a45298e303363b4451664dc1a
Margarita Busarova <margarita.busarova13@gmail.com>
2024-10-27 15:18:38 +0300

    Сделана ЛБ1

--- Desktop/Window1.xaml.cs
+++ Desktop/Window1.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,27 +10,26 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Desktop
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
         }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Window1 win1 = new Window1();
-            win1.Show();
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
             this.Close();
         }
-    
         private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
             UpdateWatermark();
@@ -45,5 +48,22 @@ namespace Desktop
             watermark1.Visibility = string.IsNullOrWhiteSpace(textBox1.Text) ? Visibility.Visible : Visibility.Collapsed;
         }
 
+        private void TextBox_TextChanged2(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            UpdateWatermark2();
+        }
+        private void UpdateWatermark2()
+        {
+            watermark2.Visibility = string.IsNullOrWhiteSpace(textBox2.Text) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void TextBox_TextChanged3(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            UpdateWatermark3();
+        }
+        private void UpdateWatermark3()
+        {
+            watermark3.Visibility = string.IsNullOrWhiteSpace(textBox3.Text) ? Visibility.Visible : Visibility.Collapsed;
+        }
     }
 }
